d468b6f4eeb8ab31843fc0cf1ce8b7e39e1fca11
Angelo Guiao <angeloguiao.personal@gmail.com>
2025-07-07 23:09:57 +0800

    bg

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,13 @@ import { LoginForm } from "@/components/login-form"
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
+       <div className="bg-muted relative hidden lg:block">
+        <img
+          src={bg}
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
@@ -20,13 +27,7 @@ export default function LoginPage() {
           </div>
         </div>
       </div>
-      <div className="bg-muted relative hidden lg:block">
-        <img
-          src={bg}
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
-      </div>
+
     </div>
   )
 }
